45033d9ed6d15f048f720a534114d8154488816e
carwash <carwash@localhost>
2016-02-22 18:50:59 -0500

    New version 2.1.

--- src/gov/nist/appvet/shared/backend/AppVetProperties.java
+++ src/gov/nist/appvet/shared/backend/AppVetProperties.java
@@ -34,7 +34,7 @@ import java.util.ArrayList;
 public class AppVetProperties {
 	public static boolean error = false;
 	/** AppVet Github release version number. */
-	public static final String APPVET_VERSION = "2.0.7"; 
+	public static final String APPVET_VERSION = "2.1"; 
 	// Logging
 	public static Logger log = null;
 	private static String APPVET_LOG_NAME = "appvet_log.txt";
